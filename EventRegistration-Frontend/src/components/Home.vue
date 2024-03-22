52306946329c809e7302a9fef9bba3f802ff2fed
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2024-03-22 10:56:55 -0400

    Add some dynamic behaviour to the home component

--- EventRegistration-Frontend/src/components/Home.vue
+++ EventRegistration-Frontend/src/components/Home.vue
@@ -3,14 +3,14 @@
         <h1>Event Registration</h1>
         <h2>New In-Person Event</h2>
         <div>
-            <input type="text" placeholder="Name" />
-            <input type="date" placeholder="Date" />
-            <input type="time" placeholder="Start Time" />
-            <input type="time" placeholder="End Time" />
-            <input type="text" placeholder="Limit" />
-            <input type="text" placeholder="Address" />
-            <button id="create-btn">Create</button>
-            <button class="danger-btn">Clear</button>
+            <input type="text" placeholder="Name" v-model="newEventName" />
+            <input type="date" placeholder="Date" v-model="newEventDate" />
+            <input type="time" placeholder="Start Time" v-model="newEventStartTime" />
+            <input type="time" placeholder="End Time" v-model="newEventEndTime" />
+            <input type="text" placeholder="Limit" v-model="newEventLimit" />
+            <input type="text" placeholder="Location" v-model="newEventLocation" />
+            <button @click="createEvent()" v-bind:disabled="isCreateBtnDisabled">Create</button>
+            <button class="danger-btn" @click="clearInputs()">Clear</button>
         </div>
         <h2>Events</h2>
         <table>
@@ -20,10 +20,10 @@
                     <th>Date</th>
                     <th>Limit</th>
                 </tr>
-                <tr>
-                    <td>McGill Juggling Fest</td>
-                    <td>2024-03-22</td>
-                    <td>100</td>
+                <tr v-for="e in events">
+                    <td>{{ e.name }}</td>
+                    <td>{{ e.date }}</td>
+                    <td>{{ e.limit ? e.limit : "-" }}</td>
                 </tr>
             </tbody>
         </table>
@@ -31,7 +31,53 @@
 </template>
 
 <script>
-
+export default {
+    name: "Home",
+    data() {
+        return {
+            events: [
+                { name: "McGill Juggling", date: "2024-03-22", startTime: "9:00:00", endTime: "21:00:00", limit: 100, location: "McGill" },
+                { name: "Solar Eclipse", date: "2024-04-08", startTime: "14:15:00", endTime: "16:30:00", limit: null, location: "Montreal" },
+            ],
+            newEventName: null,
+            newEventDate: null,
+            newEventStartTime: null,
+            newEventEndTime: null,
+            newEventLimit: null,
+            newEventLocation: null
+        };
+    },
+    methods: {
+        createEvent() {
+            const newEvent = {
+                name: this.newEventName,
+                date: this.newEventDate,
+                startTime: this.newEventStartTime,
+                endTime: this.newEventEndTime,
+                limit: this.newEventLimit,
+                location: this.newEventLocation,
+            }
+            this.events.push(newEvent);
+            this.clearInputs();
+        },
+        clearInputs() {
+            this.newEventName = null;
+            this.newEventDate = null;
+            this.newEventStartTime = null;
+            this.newEventEndTime = null;
+            this.newEventLimit = null;
+            this.newEventLocation = null;
+        }
+    },
+    computed: {
+        isCreateBtnDisabled() {
+            return (!this.newEventName
+                || !this.newEventDate
+                || !this.newEventStartTime
+                || !this.newEventLocation);
+        }
+    }
+};
 </script>
 
 <style>
@@ -55,10 +101,5 @@ th {
 .danger-btn {
     border-color: red;
     color: red;
-}
-
-#create-btn {
-    border-color: forestgreen;
-    color: forestgreen;
 }
 </style>
